98e416b31d29cf6b839753c5a0495d58d18a5b71
Saiful Islam <saifulalltime@gmail.com>
2024-02-07 21:30:22 +0600

    - Article Test added

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\ArticleController;
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,28 +17,48 @@ class ArticleControllerTest extends TestCase
      * Direct Method wise Test
     */
 
-    // This is Method Wise Test
-    public function it_shows_list_of_todo()
-    {
-        /** 
+    use RefreshDatabase;
+
+     /** 
          * @PHPunit Standard AAA (Arrange, Act, Assert)
          * @Arrange - Essential Requirements write in Arrange
          * @Act - How to Run/what we want to check
          * @Assert - Expectation 
-        */
+    */
 
 
+    // This is Method Wise Test
+    public function it_will_show_list_of_article()
+    {
         // Arrange / Preperation / prepare   
             Article::factory()->count(10)->create();
         
         // Act / Action / perform
-        // $response = $this->getJson('todo-list');
-        // dd($response->json());
-
+        $post = (new ArticleController)->index();
 
         // Assertion  / perdict
-        // $this->assertEquals(1,count($response->json()));
-
+        $this->assertEquals(10,$post->count());
     }
-    
+
+
+    /**
+     * @test
+     */
+     // This test will test a single article
+     public function it_will_show_a_single_of_article()
+     {
+        // Arrange / Preperation / prepare   
+        $post = Article::factory()->create([
+            'title' => 'This is a single article title',
+            'body' => 'This is a single article body',
+        ]);
+         
+        // Act / Action / perform
+        $getPost = (new ArticleController)->show($post->id);
+
+        // Assertion  / perdict
+        $this->assertEquals($post->id, $getPost['id']);
+        $this->assertEquals('This is a single article title', $getPost['title']);
+     }
+     
 }
